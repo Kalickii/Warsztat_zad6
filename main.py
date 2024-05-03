466e183c41295ff68a30a7ff5802e852cb9531de
Michał <michal.kalicki@outlook.com>
2024-05-03 10:23:25 +0200

    Dice function correction.

--- main.py
+++ main.py
@@ -4,37 +4,29 @@ from random import randint
 def dice(code):
     """
     Dice roll.
-    User input, how many throws, which type of dice, and additional modifier he want to use in the simulation.
+    User input which type of dice he want to throw.
     :param code: xDy+z  where: x = throws, Dy = type of dice, z = additional modifier ex. '2D10+10'
     :rtype: int
-    :return: Function returns simulation of the dice roll.
+    :return: Function returns the result of two throws of the chosen dice.
     """
     dice_types = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
-    throws = 0
+    throws = 2
     valid_dice = 0
-    modifier = 0
-
-    try:
-        found_dice = re.findall(r"D\d+", code)[0]
-        if found_dice in dice_types:
+    result = 0
+    found_dice = re.findall(r"D\d+", code)[0]
+    while found_dice in dice_types:
+        try:
             valid_dice = int(found_dice[1::])
+        except IndexError:
+            print("Invalid code")
+        except TypeError:
+            print("Invalid code")
+        for x in range(throws):
+            result += randint(1, valid_dice)
+        return result
+    if found_dice not in dice_types:
+        return "Invalid dice type"
 
-        if "D" == code[0]:
-            throws = 1
-        else:
-            throws = int(re.findall(r"\d+", code)[0])
-
-        modifier = int(re.findall(r"\d+", code)[-1])
-        if "-" in code:
-            modifier *= -1
-        if "-" not in code and "+" not in code:
-            modifier = 0
-    except IndexError:
-        print("Invalid code")
-    except TypeError:
-        print("Invalid code")
-    result = throws * randint(1, valid_dice) + modifier
-    return result
 
 player_points = 0
 computer_points = 0
