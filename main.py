98907a2de82afbc93a4e89bcee6f493a6984e553
Michał <michal.kalicki@outlook.com>
2024-05-07 10:19:45 +0200

    Dice function changed, comp_dice function created, Modification 1 Done

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import re
-from random import randint
+from random import randint, choice
 
-def dice(code):
+def dice():
     """
     Dice roll.
     User input which type of dice he want to throw.
-    :param code: xDy+z  where: x = throws, Dy = type of dice, z = additional modifier ex. '2D10+10'
     :rtype: int
     :return: Function returns the result of two throws of the chosen dice.
     """
@@ -13,45 +12,62 @@ def dice(code):
     throws = 2
     valid_dice = 0
     result = 0
-    found_dice = re.findall(r"D\d+", code)[0]
-    while found_dice in dice_types:
+    code = ""
+
+    while code not in dice_types:
         try:
-            valid_dice = int(found_dice[1::])
+            code = input("Enter a dice type: ")
+            if code in dice_types:
+                valid_dice = int(code[1::])
+            else:
+                print("Invalid dice type")
+        except ValueError:
+            print("Invalid code")
         except IndexError:
             print("Invalid code")
         except TypeError:
             print("Invalid code")
-        for x in range(throws):
-            result += randint(1, valid_dice)
-        return result
-    if found_dice not in dice_types:
-        return "Invalid dice type"
 
+    for x in range(throws):
+        result += randint(1, valid_dice)
+    return result
+
+def comp_dice():
+    """
+    Computer dice roll.
+    Function randomly picks computer dice type and simulates the throws.
+    :rtype: int
+    :return: Function returns the result of two throws of the chosen dice.
+    """
+    dice_types = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
+    throws = 2
+    valid_dice = 0
+    result = 0
+
+    code = choice(dice_types)
+    valid_dice = int(code[1::])
+
+    for x in range(throws):
+        result += randint(1, valid_dice)
+    return result
 
 player_points = 0
 computer_points = 0
 
-play = input("Press Enter to throw the dices: ")
-while play != "":
-    play = input("Press Enter! ")
+dice_types = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
+print(dice_types)
 
-throw = randint(1, 6) + randint(1, 6)
+throw = randint(1, dice()) + randint(1, dice())
 player_points += throw
 
-comp_throw = randint(1, 6) + randint(1, 6)
+comp_throw = randint(1, comp_dice()) + randint(1, comp_dice())
 computer_points += comp_throw
 
 print(f"Your score is: {player_points}")
 print(f"Computer score is: {computer_points}")
 
-play = input("Press Enter to throw the dices: ")
 while player_points < 2001 and computer_points < 2001:
-    throw = 0
-    comp_throw = 0
-    while play != "":
-        play = input("Press Enter! ")
-
-    throw = randint(1, 6) + randint(1, 6)
+    throw = randint(1, dice()) + randint(1, dice())
     if throw == 7:
         player_points //= throw
     elif throw == 11:
@@ -59,7 +75,7 @@ while player_points < 2001 and computer_points < 2001:
     else:
         player_points += throw
 
-    comp_throw = randint(1, 6) + randint(1, 6)
+    comp_throw = randint(1, comp_dice()) + randint(1, comp_dice())
     if comp_throw == 7:
         computer_points //= comp_throw
     elif comp_throw == 11:
@@ -76,5 +92,3 @@ while player_points < 2001 and computer_points < 2001:
     elif computer_points >= 2001:
         print("You lose!")
         break
-    play2 = input("Press Enter to throw the dices: ")
-
